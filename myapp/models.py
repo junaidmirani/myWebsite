bd1582a7ccb7928e4148f932ff80fee530c4cd81
junaid liaqat <miranijunaid423@gmail.com>
2024-06-16 12:21:55 +0500

    intial commit

---
+++ myapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', null= True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
+    writer_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
